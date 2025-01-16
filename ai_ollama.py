11183e82c3cb2505c92a2daa7845debd492f403c
Manasvi Kotian <kotianmanasvi98@gmail.com>
2025-01-16 03:38:47 +0100

    Added comments

--- ai_ollama.py
+++ ai_ollama.py
@@ -1,6 +1,7 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
 
+# Template for the chatbot to follow the conversation
 template = """
 Answer the question below.
 
@@ -11,20 +12,27 @@ Question: {question}
 Answer:
 """
 
+# Loading llama3.2 model from Ollama
 model = OllamaLLM(model="llama3.2")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
+
+# Function with an infinite loop to keep the chat going on
 def handle_conversation():
     context = ""
     print("Start chatting. Type quit, exit or bye to quit")
     while True:
         user_input = input("You: ")
+        
+        # Breaks, to stop the chat
         if user_input.lower() in ['quit', "exit", "bye"]:
             break
 
         result = chain.invoke({"context": context, "question": user_input})
         print("Bot: ", result)
+
+        # Storing chat history for the Bot to continue the conversation
         context += f"\nUser: {user_input}\nAI: {result}"
     
 
